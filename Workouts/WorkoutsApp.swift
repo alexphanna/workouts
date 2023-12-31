83b29157df1867dbc5ffe0326b7e19d3f3dd1133
alexphanna <ahanna0608@gmail.com>
2023-12-31 00:35:09 -0500

    Initial Commit

---
+++ Workouts/WorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutsApp.swift
+//  Workouts
+//
+//  Created by Alex on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
