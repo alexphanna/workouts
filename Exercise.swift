cbe1022e45c3467332749d468ee037156ed02a1b
alexphanna <ahanna0608@gmail.com>
2024-01-03 21:59:10 -0500

    Add more basic functionality

---
+++ Exercise.swift
@@ -0,0 +1,8 @@
+//
+//  Exercise.swift
+//  Workouts
+//
+//  Created by Alex on 1/3/24.
+//
+
+import Foundation
---
+++ ExerciseView.swift
@@ -0,0 +1,8 @@
+//
+//  ExerciseView.swift
+//  Workouts
+//
+//  Created by Alex on 1/3/24.
+//
+
+import Foundation
