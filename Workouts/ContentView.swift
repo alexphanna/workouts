c2b1171e64ccbac4529bebe41e422f973c1eb5c3
alexphanna <ahanna0608@gmail.com>
2024-02-25 15:45:34 -0500

    Update workout deletion

--- Workouts/ContentView.swift
+++ Workouts/ContentView.swift
@@ -12,83 +12,105 @@ import CoreData
 struct ContentView: View {
     @State private var isShowingSheet = false
     @State private var isShowingSettingsSheet = false
+    @State private var isShowingDialog = false
     @State private var newName: String = ""
     @State private var newDate: Date = .now
     
-    @Environment(\.editMode) private var editMode
+    @State private var isEditing: EditMode = .inactive
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Workout.date, ascending: false)])
     private var workouts: FetchedResults<Workout>
+    
+    @State private var multiSelection = Swift.Set<Workout>()
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(workouts) { workout in
+            List(selection: $multiSelection) {
+                ForEach(workouts, id: \.self) { workout in
                     NavigationLink(destination: WorkoutView(workout: workout)) {
                         VStack(alignment: .leading) {
                             Text(workout.name)
-                                .bold()
-                            Text((workout.date.formatted(date: .numeric, time: .omitted)))
+                                .font(.headline)
+                            Text(workout.date.formatted(.dateTime.month(.defaultDigits).day(.defaultDigits).year(.twoDigits)))
+                                .font(.callout)
+                                .foregroundStyle(.gray)
                         }
                     }
                 }
-                .onDelete(perform: deleteWorkouts)
+                //.onDelete(perform: deleteWorkouts)
             }
             .navigationTitle("Workouts")
             .toolbar {
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
-                ToolbarItem(placement: .bottomBar) {
-                    Button(action: { isShowingSettingsSheet = true } ) {
-                        Image(systemName: "gear")
-                    }.sheet(isPresented: $isShowingSettingsSheet) {
-                        SettingsView()
+                if isEditing == .inactive {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: { isShowingSettingsSheet = true } ) {
+                            Image(systemName: "gear")
+                        }.sheet(isPresented: $isShowingSettingsSheet) {
+                            SettingsView()
+                        }
                     }
-                }
-                ToolbarItem(placement: .bottomBar) {
-                    Button(action: { isShowingSheet = true }) {
-                        Image(systemName: "plus")
-                    }
-                    .sheet(isPresented: $isShowingSheet) {
-                        NavigationView {
-                            Form {
-                                TextField("Name", text: $newName)
-                                DatePicker("Date", selection: $newDate, displayedComponents: [.date])
-                            }
-                            .toolbar {
-                                ToolbarItem(placement: .cancellationAction) {
-                                    Button("Cancel", action: { isShowingSheet = false })
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: { isShowingSheet = true }) {
+                            Image(systemName: "plus")
+                        }
+                        .sheet(isPresented: $isShowingSheet) {
+                            NavigationView {
+                                Form {
+                                    TextField("Name", text: $newName)
+                                    DatePicker("Date", selection: $newDate, displayedComponents: [.date])
                                 }
-                                ToolbarItem(placement: .principal) {
-                                    Text("Add Exercise")
-                                        .font(.headline)
-                                }
-                                ToolbarItem(placement: .confirmationAction) {
-                                    Button("Done", action: addWorkout )
-                                        .disabled(newName.isEmpty)
+                                .toolbar {
+                                    ToolbarItem(placement: .cancellationAction) {
+                                        Button("Cancel", action: { isShowingSheet = false })
+                                    }
+                                    ToolbarItem(placement: .principal) {
+                                        Text("Add Exercise")
+                                            .font(.headline)
+                                    }
+                                    ToolbarItem(placement: .confirmationAction) {
+                                        Button("Done", action: addWorkout )
+                                            .disabled(newName.isEmpty)
+                                    }
                                 }
                             }
                         }
                     }
+                    ToolbarItem(placement: .status) {
+                        if workouts.count == 0 {
+                            Text("No Workouts")
+                                .font(.caption)
+                        }
+                        else if workouts.count == 1 {
+                            Text(workouts.count.description + " Workout")
+                                .font(.caption)
+                        }
+                        else {
+                            Text(workouts.count.description + " Workouts")
+                                .font(.caption)
+                        }
+                    }
                 }
-                ToolbarItem(placement: .status) {
-                    if workouts.count == 0 {
-                        Text("No Workouts")
-                            .font(.caption)
-                    }
-                    else if workouts.count == 1 {
-                        Text(workouts.count.description + " Workout")
-                            .font(.caption)
-                    }
-                    else {
-                        Text(workouts.count.description + " Workouts")
-                            .font(.caption)
+                else if isEditing == .active {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Spacer()
+                        if multiSelection.count == 0 {
+                            Button("Delete All", action: { isShowingDialog = true } )
+                                .confirmationDialog("", isPresented: $isShowingDialog) {
+                                    Button("Delete All", role: .destructive, action: { deleteAllWorkouts() })
+                                }
+                        }
+                        else {
+                            Button("Delete", action: { deleteWorkouts(workouts: multiSelection)} )
+                        }
                     }
                 }
             }
+            .environment(\.editMode, $isEditing)
         }
     }
 
@@ -112,10 +134,24 @@ struct ContentView: View {
         }
     }
 
-    private func deleteWorkouts(offsets: IndexSet) {
+    private func deleteWorkouts(workouts: Swift.Set<Workout>) {
         withAnimation {
-            offsets.map { workouts[$0] }.forEach(viewContext.delete)
+            workouts.map { $0 }.forEach(viewContext.delete)
 
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    private func deleteAllWorkouts() {
+        withAnimation {
+            for workout in workouts {
+                viewContext.delete(workout)
+            }
             do {
                 try viewContext.save()
             } catch {
